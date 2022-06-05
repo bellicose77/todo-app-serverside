cc8f47043691a141ad9a00c0eb0fc39f149a1e95
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-06 00:52:20 +0600

    async function

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const { MongoClient} = require('mongodb');
 
 const app = express();
 const port = 5000;
-WpK3TYgZfdkd5Rpx
-TODOdb
+
 const uri = "mongodb+srv://TODOdb:WpK3TYgZfdkd5Rpx@cluster0.vtwog.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+    const collection = client.db("Task").collection("TaskInfo");
+    // perform actions on the collection object
+    console.log("hitteed data base");
+    client.close();
+  });
+  async function run()
+  {
+      
+  }
+
 app.get('/',(req,res)=>{
     res.send("hello world");
 });
