7a718fc622ba49a975d3382aa7429a59c5276071
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-06 00:59:16 +0600

    run function

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const port = 5000;
 
 const uri = "mongodb+srv://TODOdb:WpK3TYgZfdkd5Rpx@cluster0.vtwog.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-client.connect(err => {
-    const collection = client.db("Task").collection("TaskInfo");
-    // perform actions on the collection object
-    console.log("hitteed data base");
-    client.close();
-  });
   async function run()
   {
       try{
         await client.connect(); 
+        const database = client.db("todoDB");
+        const todo = database.collection("todo");
+        app.get('/',(req,res)=>{
+            res.send("hello world");
+        });
+        
+        app.post('/task',(req,res)=>{
+            console.log("hitting ta post");
+        })
       }
       finally {
         await client.close();
@@ -24,12 +27,6 @@ client.connect(err => {
   }
   run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send("hello world");
-});
 
-app.post('/task',(req,res)=>{
-    console.log("hitting ta post");
-})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)})
