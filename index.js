b834193c20722ff477c4a4c155b0ed54f281fcaa
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-06-07 20:55:05 +0600

    error

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { MongoClient} = require('mongodb');
+const { MongoClient,ServerApiVersion } = require('mongodb');
+const app = express();
 const cors = require('cors');
 
-const app = express();
+
 app.use(cors());
 app.use(express.json());
 const port = 5000;
 
 const uri = "mongodb+srv://TODOdb:WpK3TYgZfdkd5Rpx@cluster0.vtwog.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true,serverApi: ServerApiVersion.v1 });
   async function run()
   {
       try{
@@ -17,8 +18,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
         const todo = database.collection("todo");
         // console.log("hitted db")
         app.post('/task',async (req,res)=>{
-            const task = req.body;
-            const result = await todo.insertOne(task);
+            const dataInput = req.body;
+            const result = await todo.insertOne(dataInput);
             console.log("hitting ta post",result);
             res.json(result);
         })
